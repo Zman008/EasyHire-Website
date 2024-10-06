84f131fd9afce7d4a87e0da126fa43bc83aa4b97
NuranSami <zaman.sami8@gmail.com>
2024-10-06 15:26:54 +0600

    Add files via upload

--- check_login.php
+++ check_login.php
@@ -2,6 +2,8 @@
     session_start();
     if (isset($_SESSION['user_id'])) {
         header("Location: user_profile.php");
+    } else if (isset($_SESSION['provider_id'])) {
+        header("Location: provider_profile.php");
     } else {
         header("Location: login.php");
     }
